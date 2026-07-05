c63ebe309099d493ad1c2a68317376107cdd9d5b
Opera1235 <festival2night@gmail.com>
2026-07-05 21:11:05 +0800

    feat: Building Javascript Practice.

---
+++ Javascript 練習/JavaScript Trivia Bot.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "John";
+let botLocation = "the internet";
+let favoriteLanguage = "Python";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".")
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is named after the British comedy group Monty Python, not the snake.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is known for its simplicity and readability, making it a great choice for beginners.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " has a large and active community, which means there are plenty of resources and libraries available for developers.";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
---
+++ Javascript 練習/Sentence Maker.js
@@ -0,0 +1,26 @@
+let adjective;
+let adjective2;
+let noun;
+let noun2;
+let verb;
+let place;
+
+adjective = "fierce";
+adjective2 = "tiny";
+noun = "dragon";
+noun2 = "sheep";
+verb = "sleeping";
+place = "mountain";
+
+let firstStory = 'Once upon a time, there was a(n) ' + adjective + ' ' + noun + ' who loved to eat ' + noun2 + '. The ' + noun + ' lived in a ' + place + ' and had ' + adjective2 + ' nostrils that blew fire when it was ' + verb + '.';
+console.log('First story: ' + firstStory);
+
+adjective = 'brave';
+adjective2 = 'sleepy';
+noun = 'lion';
+noun2 = 'elephant';
+verb = 'roars';
+place = 'jungle';
+
+let secondStory = 'Once upon a time, there was a(n) ' + adjective + ' ' + noun + ' who loved to eat ' + noun2 + '. The ' + noun + ' lived in a ' + place + ' and had ' + adjective2 + ' nostrils that blew fire when it was ' + verb + '.';
+console.log('Second story: ' + secondStory);
